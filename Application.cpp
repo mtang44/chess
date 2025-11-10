1709974bee8f0b824644a64399ca1bffe6e54e98
mcst2014 <mcst2014@outlook.com>
2025-11-09 17:44:11 -0800

    logger added

--- Application.cpp
+++ Application.cpp
@@ -6,10 +6,20 @@
 #include "classes/Connect4.h"
 #include "classes/Chess.h"
 
+
+#include <string>
+#include "Logger.h"
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
 namespace ClassGame {
         //
         // our global variables
         //
+        bool LogWindowVisible = false;
+        bool playerTypeSelected = false;
+        void showLogWindow(bool* p_open);
         Game *game = nullptr;
         bool gameOver = false;
         int gameWinner = -1;
@@ -20,6 +30,10 @@ namespace ClassGame {
         //
         void GameStartUp() 
         {
+            Logger& logger = Logger::GetInstance();
+            logger.LogInfo("Game started successfully");
+
+            logger.LogGameEvent("Application initialized");
             game = nullptr;
         }
 
@@ -42,6 +56,7 @@ namespace ClassGame {
                         game->stopGame();
                         game->setUpBoard();
                         gameOver = false;
+                        playerTypeSelected = false;
                         gameWinner = -1;
                     }
                 }
@@ -88,6 +103,30 @@ namespace ClassGame {
                     game->drawFrame();
                 }
                 ImGui::End();
+                  ImGui::Begin("Log Window");
+               
+                showLogWindow(&LogWindowVisible);
+                for(LogStuff s: Logger::GetInstance().log)
+                {
+                    if(s.level == LogStuff::ERROR)
+                    {
+                        ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 0.0f, 0.0f, 1.0f));
+                        ImGui::Text("%s: %s", s.timeStamp.c_str(), s.loggingMessage.c_str());
+                        ImGui::PopStyleColor();
+                    }
+                    else if(s.level == LogStuff::WARNING)
+                    {
+
+                        ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 0.0f, 1.0f));
+                        ImGui::Text("%s: %s", s.timeStamp.c_str(), s.loggingMessage.c_str());
+                        ImGui::PopStyleColor();
+                    }
+                    else {
+                        ImGui::Text("%s: %s", s.timeStamp.c_str(), s.loggingMessage.c_str());
+                    }
+                }
+                ImGui::End();
+                
         }
 
         //
@@ -107,4 +146,69 @@ namespace ClassGame {
                 gameWinner = -1;
             }
         }
+        void showLogWindow(bool* p_open)
+        {
+           
+            const bool output_to_file = ImGui::Button("Output to File"); ImGui::SameLine();
+            const bool clear_btn = ImGui::Button("clear"); ImGui::SameLine();
+            const bool test_info_btn = ImGui::Button("Test Info"); ImGui::SameLine();
+            const bool test_warning_btn = ImGui::Button("Test Warning"); ImGui::SameLine();
+            const bool test_error_btn = ImGui::Button("Test Error");
+              if(output_to_file)
+            {
+               Logger::GetInstance().LogInfo("Outputting to log file");
+               ofstream logFile("game_log.txt",ios::out);
+               if(logFile.is_open())
+               {
+                 Logger::GetInstance().LogInfo("Adding log messages to log");;
+                   for(LogStuff s: Logger::GetInstance().log)
+                   {
+                       logFile << s.timeStamp << " " << s.loggingMessage << endl;
+                   }
+                   logFile.close();
+                   std::cout << "file located at : " << std::filesystem::current_path() << '\n';
+                   Logger::GetInstance().LogInfo("Log file created: game_log.txt");                   
+               }
+               else
+               {
+                    Logger::GetInstance().LogInfo("File failed to open");
+               }
+            }
+            if(clear_btn)
+            {
+               Logger::GetInstance().log.clear();
+            }
+            if(test_info_btn)
+            {
+                Logger::GetInstance().LogInfo("This is a test Info message.");
+            }
+            if(test_warning_btn)
+            {
+                Logger::GetInstance().LogWarning("This is a test Warning message.");
+
+            }
+            if(test_error_btn)
+            {
+                Logger::GetInstance().LogError("This is a test Error message.");
+            }
+        }   
+        void ToggleLogWindow()
+        {
+            if(!IsLogWindowVisible() )
+            {
+                cout << "Log window was false setting to visible" << endl;
+                LogWindowVisible = true;
+                  cout << "Log window set to visible" << endl;
+                 showLogWindow(&LogWindowVisible);
+            }
+            else
+            {
+                LogWindowVisible = false;
+            }
+        }
+        bool IsLogWindowVisible()
+        {
+            return LogWindowVisible;
+        }
 }
+
--- Application.h
+++ Application.h
@@ -4,4 +4,9 @@ namespace ClassGame {
     void GameStartUp();
     void RenderGame();
     void EndOfTurn();
+
+
+    void ShowLogWindow(bool* p_open = nullptr);
+    void ToggleLogWindow();
+    bool IsLogWindowVisible();
 }
