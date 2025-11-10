0f2cda57938dc9b231a0b8ac8d5bd2f3e59ded4e
mcst2014 <mcst2014@outlook.com>
2025-11-09 17:31:05 -0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mcst2/OneDrive/Documents/Desktop/cmpm123_test/Assignment 5 Chess/chess
+# Build directory: C:/Users/mcst2/OneDrive/Documents/Desktop/cmpm123_test/Assignment 5 Chess/chess/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
